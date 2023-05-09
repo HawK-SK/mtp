f599cf509589a5f37876eb590bc948e49ed37d2d
HawK-SK <111142859+HawK-SK@users.noreply.github.com>
2023-05-09 14:30:55 +0530

    new features added and the requirements.txt

--- MTP.py
+++ MTP.py
@@ -9,13 +9,17 @@ from pyfiglet import figlet_format
 
 cprint(figlet_format('MobThePhone'))
 
+print("Enter the number with the country code! ")
 phoneNumber = phonenumbers.parse(input("Enter the phone number: "))
 
+# Features
+valid = phonenumbers.is_valid_number(phoneNumber, 'en')
 Carrier = carrier.name_for_number(phoneNumber, 'en')
-
+Timezone = timezone.time_zones_for_number(phoneNumber, 'en')
 Region = geocoder.description_for_number(phoneNumber, 'en')
 
-
-
+# On-screen print
+print(f'Validation: {valid}')
 print(f'Opretor: {Carrier}')
+print(f'Timezone: {Timezone}')
 print(f'Region: {Region}')
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-MobThePhone
+------MobThePhone------
+
+This is a simple program to get some information about a phone number.
+
+Features: 
+    # Phone number Validation
+    # Phone number's Opreator
+    # Phone number's Time zone
+    # Phone number's Region
+
+
+Steps: 
+    1. Open mtp.py
+    2. Enter phone number with the country code (ex -- +1(phonenumber)) // replace (phonenumber) with the number you want to check
+    3. Press enter
+    4. Wait
+    5. And you are done
+
+Have fun!
